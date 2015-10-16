e8a231006042a7638e7c47c41bce08b9003a6858
Iakov Davydov <iakov.davydov@unil.ch>
2015-10-16 19:22:25 +0200

    add fast_te: extract tree in fastcodeml format

---
+++ fast_te.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Newick.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		cerr << "Please specify file name\n";
+		return 1;
+	}
+	Newick tree(0);
+	tree.readFile(argv[1]);
+	tree.printTreeAnnotated(cout);
+}
