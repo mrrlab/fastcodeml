14e81146fa37bfcb495d49323f3940f7f65f5fbb
Iakov Davydov <iakov.davydov@unil.ch>
2015-12-03 16:54:48 +0100

    fix format.sh to not format nlopt.h

--- format.sh
+++ format.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls *.cpp *.h | grep -Ev "^(blas[.]h|lapack[.]h|nlopt[.h])$" | xargs clang-format-3.6 -i
+ls *.cpp *.h | grep -Ev "^(blas[.]h|lapack[.]h|nlopt[.]h)$" | xargs clang-format-3.6 -i
